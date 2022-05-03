c27bab472f22415ad21b05dccaf15f0fe70c9f2c
Cansu <moran.cansu@gmail.com>
2022-05-03 03:38:54 +0300

    retrieve faces from database added

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ import classification
 import detection
 import base64
 import blending
-from io import BytesIO
-import io
 import json
 import cv2
 import numpy as np
+import firebase_connection
 
 app = Flask("__name__")
 cors = CORS(app)
@@ -51,6 +50,10 @@ def replace():
 
             #draw.rectangle([(left, top), (right, bottom)], outline="red", fill="magenta")
             #image.save("isitface{}.jpg".format(face), "JPEG")
+
+            #adjust bounding box
+            #todo: ideas about improving blending:
+            #if the facebox is too big, it can't detect -> resize the box? (smaller)
             if (left - 20) > 0:
                 left -= 20
             else:
@@ -90,9 +93,10 @@ def replace():
             print("height", face[index]["height"])
             print("gender", face[index]["gender"])
 
-            #todo: retrieve image from database here (called db_image)
-            db_image = Image.open("src.jpg").convert('RGB')
-            blended = blending.blend_faces(db_image, single_face.convert("RGB"))#single_face.convert('RGB')) #is in PIL IMAGE FORMAT
+            #retrieve image from database here (called db_image)
+            db_image = firebase_connection.retrieve_image_from_database(age, gender, race)
+
+            blended = blending.blend_faces(db_image.convert("RGB"), single_face.convert("RGB"))#single_face.convert('RGB')) #is in PIL IMAGE FORMAT
             if blended is None:
                 print("no destination face!!!!!!")
                 face[index]["invalid"] = True
@@ -123,24 +127,81 @@ def replace():
     return jsonify(result)
 
 @app.route("/blend", methods=['POST'])
-#TODO: take the entire image and crop to blend - put the blend back in the image - return entire image
 def blend():
-    src_image = Image.open("src.jpg").convert('RGB')
-    dst = request.files.get('dst_image', '')
-    dst_image = Image.open(dst, mode='r').convert('RGB')
-    output = blending.blend_faces(src_image, dst_image)
-    """img_io = BytesIO()
-    output.save(img_io, 'JPEG', quality=70)
-    img_io.seek(0)"""
+    imagefile = request.files.get('image', '')
+    image = Image.open(imagefile, mode='r')
+    faces_str = request.form.get('faces')
+    print(faces_str)
+    faces = json.loads(faces_str)
+    print(faces)
 
-    #np_img = np.array(output)
-    success, encoded_image = cv2.imencode('.png', output)
+    for face in faces:
+        if face is None:
+            print("null value here!")
+            continue
+        for index in face:
+            print(face[index])
+            print(index)
+            properties = face[index]
+            left = properties["left"]
+            top = properties["top"]
+            right = properties["left"] + properties["width"]
+            bottom = properties["top"] + properties["height"]
+            age = properties["age"]
+            gender = properties["gender"]
+            race = properties["skinColor"]
+
+            #adjust bounding box
+            if (left - 20) > 0:
+                left -= 20
+            else:
+                left = 0
+
+            if right + 20 < image.width:
+                right = right + 20
+            else:
+                right = image.width - 1
+
+            if (top - 20) > 0:
+                top -= 20
+            else:
+                top = 0
+
+            if bottom + 20 < image.height:
+                bottom = bottom + 20
+            else:
+                bottom = image.height - 1
+
+            single_face = image.crop((left, top, right, bottom))
+
+            #retrieve image from database here (called db_image)
+            db_image = firebase_connection.retrieve_image_from_database(age, gender, race)
+
+            blended = blending.blend_faces(db_image.convert("RGB"), single_face.convert("RGB"))#single_face.convert('RGB')) #is in PIL IMAGE FORMAT
+            if blended is None:
+                print("no destination face!!!!!!")
+                face[index]["invalid"] = True
+            else:
+                face[index]["invalid"] = False
+                image.paste(blended, (left, top))
+                image.save('output_blended_blend{}.png'.format(index))
+
+    #convert image(PIL) to numpy
+    print("image size before sending", image.size)
+    rgb_img = image.convert("RGB")
+    np_image = np.array(rgb_img)
+    np_image = np_image[:, :, : :-1].copy()
+
+    #encode numpy image
+    print("numpy before encoding size", np_image.shape)
+    success, encoded_image = cv2.imencode('.png', np_image)
     content = encoded_image.tobytes()
     response_img = base64.b64encode(content).decode('ascii')
-    result = {"image":response_img}
-    return jsonify(result)
 
-    #return send_file(img_io, mimetype='image/jpeg')
+    print("final faces:", faces)
+    result = {"image": response_img, "faces": faces}
+
+    return jsonify(result)
 
 class InvalidUsage(Exception):
     status_code = 400
